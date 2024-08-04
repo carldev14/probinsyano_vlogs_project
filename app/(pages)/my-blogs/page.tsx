eeb756c6424460717695307617b8b9014befb0a7
carldev14 <carldev.ac@gmail.com>
2024-08-04 22:18:45 +0800

    use useRouter instead of Link

--- app/(pages)/my-blogs/page.tsx
+++ app/(pages)/my-blogs/page.tsx
@@ -3,17 +3,20 @@
 import Loading from "@/components/loading";
 import GreetTemplate from "@/templates/greet_template";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const BlogUi = dynamic(() => import('@/components/blog_ui'), {
     loading: () => <Loading />,
     ssr: false, // Set to true for server-side rendering (optional)
-  });
+});
 export default function BlogsList() {
     return (
         <div className="p-3 place-items-center grid ">
             <section className="flex flex-col md:w-4/5 w-full gap-2">
                 <GreetTemplate title="Read my blogs" descriptions="I upload is mostly my daily life, recipe, and others." />
-                <BlogUi />
+                <Suspense>
+                    <BlogUi />
+                </Suspense>
             </section>
         </div>
 
--- components/greetings.tsx
+++ components/greetings.tsx
@@ -49,7 +49,7 @@ export default function Greetings() {
             )
           } else {
             return (
-              <Link href={item.url} className="p-[10px] rounded-full  text-xs text-white bg-blue-500" prefetch>
+              <Link href={item.url} className="p-[10px] rounded-full  text-xs text-white bg-blue-500" prefetch={true}>
                 {item.name}
               </Link>
             )
