3ad5b8f80fe7e845198d155aeb9f33e51ac941d1
carldev14 <carldev.ac@gmail.com>
2024-08-09 23:21:54 +0800

    Update blog_page_body_tutorial.tsx

--- components/blog_page_body_tutorial.tsx
+++ components/blog_page_body_tutorial.tsx
@@ -11,11 +11,11 @@ export default function BlogPageBodyTut({ content_one, content_two, image_two, u
             <div className="w-full md:w-5/6  flex flex-col gap-6  p-2">
 
                 <section className="flex flex-col md:gap-1 gap-3 ">
-                    <h1 className={` text-xl text-black/80`}>{title_step_one}</h1>
+                    <h1 className={` md:text-xl text-base text-black/80`}>{title_step_one}</h1>
                     <p className={`${smallfontFace.className} text-[15.5px] text-black/80 `}>{content_one}</p>
                 </section>
                 <section className="flex flex-col md:gap-1 gap-3">
-                    <h1 className={`text-xl text-black/80`}>{title_step_two}</h1>
+                    <h1 className={`md:text-xl text-base text-black/80`}>{title_step_two}</h1>
                     <p className={`${smallfontFace.className} text-[15.5px] text-black/80 `}>{content_two}</p>
                 </section>
                 <div className="shadow shadow-neutral-300 h-1 rounded-xl"></div>
